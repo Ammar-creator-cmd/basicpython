67944b1f3e1ebedf6187210c307f439948f9061f
Ammar-creator-cmd <rokhimalusidr03@gmail.com>
2025-05-03 07:14:22 +0200

    file handdlingkt

--- file handling/readwriteappaendfiles.py
+++ file handling/readwriteappaendfiles.py
@@ -1,16 +1,16 @@
 def write(textfile, text):
     try:
-        with open(textfile, "w") as file:
+        with open(textfile, 'w') as file:
             file.write(text)
         print(f"Text written to {textfile} successfully.")
     except Exception as e:
         print(f"An error occurred while writing to {textfile}: {e}")
 
-"""write("test.txt", "This is a test file.")"""
+#write('test.txt', 'Hello, World! amar')
 
 def read(textfile):
     try:
-        with open(textfile, "r") as file:
+        with open(textfile, 'r') as file:
             content = file.read()
         print(f"Content of {textfile}:")
         print(content)
@@ -18,13 +18,55 @@ def read(textfile):
         print(f"The file {textfile} does not exist.")
     except Exception as e:
         print(f"An error occurred while reading {textfile}: {e}")
+    
+#read('test.txt')
 
-read("test.txt")
+def a():
+    f = open('song.txt', 'r')
+    data = f.read()
+    print(data)
+    f.close() # read the file
 
-def append(textfile, text):
-    try:
-        with open(textfile, "a") as file:
-            file.write(text)
-        print(f"Text appended to {textfile} successfully.")
-    except Exception as e:
-        print(f"An error occurred while appending to {textfile}: {e}")
+def length():
+    f = open('song.txt', 'r')
+    data = f.read()
+    print("there are ", len(data), "characters in the file")
+    f.close()
+
+def t():
+    f = open('song.txt', 'r')
+    data = f.readlines()
+    count = 0
+    for line in data:
+        count += 1 #counting the number of lines
+    print("Number of lines:", count) 
+    print(data)
+    f.close()
+
+def uppercasecheck():
+    f = open('song.txt', 'r')
+    data = f.read()
+    if data.isupper(): # check if the string is uppercase
+        print("The file is in uppercase.")
+    else:
+        print("The file is not in uppercase.")
+    f.close()
+
+def lowercheck():
+    f = open('song.txt', 'r')
+    data = f.read()
+    countcharacter = 0
+    for line in data:
+        if line.islower():
+            countcharacter += 1
+    print("Number of characters in lowercase:", countcharacter)   
+       
+if __name__ == '__main__':
+    # Uncomment one of the following lines to execute a function
+    #write('test.txt', 'Hello, World! amar')
+    #read('test.txt')
+    #a()
+    #length()
+    #t()
+    uppercasecheck()
+    lowercheck()
